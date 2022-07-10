a9f2fbb42f10fd778a32bd8296bacb98a7c37ac7
JacobWright0467 <jacobwright0467@gmail.com>
2022-07-09 19:37:23 -0500

    Parts 1-2 finished

--- queries.sql
+++ queries.sql
@@ -1,6 +1,11 @@
 -- Part 1: Test it with SQL
+--columns are id, employer, name, and skills
+--id is an int, the rest are varchar
 
 -- Part 2: Test it with SQL
+SELECT name
+FROM employer
+where location = "St. Louis City";
 
 -- Part 3: Test it with SQL
 
--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,35 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    public Employer() {
+    }
 
+    public Employer(String aLocation) {
+        super();
+        this.location = aLocation;
+    }
+
+    @NotBlank
+    @Size(min=3, message="Location must be between 3 and 100 characters long")
+    public String location;
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getLocation() {
+        return this.location;
+    }
 }
+
+//JAKE section 2 Models part 2 last half says Employer needs
+//non arg constructor required for Hibernate to create object
+//may need to come back to this to see if that's something you
+//missed because i'm not sure what that means.  Maybe what I
+//put on line 10?
